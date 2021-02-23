0043fb87dfed8fc8dbd35f1eebb448c6596f0302
jsmjsm <levy-pan@outlook.com>
2021-02-23 16:09:17 +0800

    Update ocsv.js

--- ocsv.js
+++ ocsv.js
@@ -17,6 +17,9 @@
 // You can optional change the response headers at the same time by using $done({body: modifiedBody, headers: modifiedHeaders}); only change the response headers is not allowed for script-response-body. The modifiedHeaders can be copied and modified from $response.headers, please do not change the content length, type and encoding field.
 // Response status can also be optional changed by using $done({body: modifiedBody, headers: modifiedHeaders, status: modifiedStatus}), the modifiedStatus should be like "HTTP/1.1 200 OK"
 
+// > URL TO MATCH: aHR0cHM6Ly92c2NvLmNvL2FwaS9zdWJzY3JpcHRpb25zLzIuMS91c2VyLXN1YnNjcmlwdGlvbnMvLio=
+// > MITM: dnNjby5jbw==
+
 var body = $response.body
 var obj = JSON.parse(body)
 
